3562187ac516ce2fdbe1d656b6ab894527c2ddd5
Gage Miller <miller2590@gmail.com>
2021-09-02 17:07:41 -0500

    initial structure

---
+++ starting/main.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+    constructor(field = [[]]) {
+        this.field = field;
+        this.locationX = 0;
+        this.locationY = 0;
+        // Set the "home" position before the game starts
+        this.field[0][0] = pathCharacter;
+    }
+
+    // Field Methods
+
+    print() {
+        // prints current state of the field
+    }
+
+    static generaterfield(height, width) {
+        // return 2 dimensional array representing the field
+    }
+
+}
